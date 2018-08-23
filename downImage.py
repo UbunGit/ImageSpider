7bbc15527031720b274a0da04cbf17f5d04b1ed8
静静地白色外套 <296019487@qq.com>
2018-08-23 12:20:25 +0800

    修改保存路径为动态

--- downImage.py
+++ downImage.py
@@ -5,11 +5,12 @@ import re
 import requests
 
 class tbimage():
-    def __init__(self,key,pageNum):
+    def __init__(self,key,pageNum,savepath):
 
         self.url = 'https://s.taobao.com/search'
         self.payload = {'q':key, 's':'1', 'ie':'utf8'}  #字典传递url参数
         self.pageNum =  int(pageNum)
+        self.savepath =  savepath
         imgelist = self.getImgeurlList()
         self.downImge(imgelist)
 
@@ -31,7 +32,7 @@ class tbimage():
         for i in range(0,len(imagelist)):
             imageurl = "http://"+ str(imagelist[i]).encode("utf-8") ;
             print(imageurl)
-            file = "data/"+"b"+str(i)+".jpg"
+            file = savepath+"/"+str(i)+".jpg"
             ir = requests.get(imageurl)
             sz = open(file, 'wb').write(ir.content)
         print("done:共计："+str(len(imagelist)))
@@ -42,18 +43,21 @@ if __name__ == '__main__':
 
     #url = "https://www.processon.com/i/570f38cbe4b0a2221694a2f5";
     print("\n")
-    print("=====根据关键字获取淘宝商品列表图片=====")
-    print("|     根据提示：                     |")
-    print("|     1：输入你的关键字               |")
-    print("|     2：输入你所需页数（每页44个）     |")
+    print("===根据关键字获取淘宝商品列表图片===")
+    print("根据提示：")
+    print("1：输入你的关键字 ")
+    print("2：输入你所需页数（每页44个）")
+    print("3：输入保存路径 ")
     print("===================================")
     print("  输入你的关键字:")
     key = raw_input()
     print("  输入你所需页数（每页44个）:")
     pagenum = raw_input()
+    print("  输入保存路径:")
+    savepath = raw_input()
     try:
         print("开始获取关键字为："+key+"的商品图片")
-        runlop = tbimage(key,pagenum)
+        runlop = tbimage(key,pagenum,savepath)
     except Exception as e:
         print("error"+str(e))
         print("================end================")
