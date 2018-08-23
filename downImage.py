8348280be23915d6b2b51252a854841d8c6c2c7d
静静地白色外套 <296019487@qq.com>
2018-08-23 11:46:56 +0800

    淘宝商品列表图片
    一个超简单的获取淘宝图片的爬虫

---
+++ downImage.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import re
+import requests
+
+class tbimage():
+    def __init__(self,key,pageNum):
+
+        self.url = 'https://s.taobao.com/search'
+        self.payload = {'q':key, 's':'1', 'ie':'utf8'}  #字典传递url参数
+        self.pageNum =  int(pageNum)
+        imgelist = self.getImgeurlList()
+        self.downImge(imgelist)
+
+
+    def getImgeurlList(self):
+
+        imagelist = []
+
+        for i in range(0,self.pageNum):        #10次，就是10页的商品数据
+
+            self.payload['s'] = 44 * i + 1   #此处改变的url参数为s，s为1时第一页，s为45是第二页，89时第三页以此类推
+            resp = requests.get(self.url, params=self.payload)
+            resp.encoding = 'utf-8'  #设置编码
+            imagelist = imagelist+re.findall(r'pic_url":"//(.*?)"', resp.text, re.I)
+        return imagelist;
+
+    def downImge(self,imagelist):
+
+        for i in range(0,len(imagelist)):
+            imageurl = "http://"+ str(imagelist[i]).encode("utf-8") ;
+            print(imageurl)
+            file = "data/"+"b"+str(i)+".jpg"
+            ir = requests.get(imageurl)
+            sz = open(file, 'wb').write(ir.content)
+        print("done:共计："+str(len(imagelist)))
+
+
+if __name__ == '__main__':
+
+
+    #url = "https://www.processon.com/i/570f38cbe4b0a2221694a2f5";
+    print("\n")
+    print("=====根据关键字获取淘宝商品列表图片=====")
+    print("|     根据提示：                     |")
+    print("|     1：输入你的关键字               |")
+    print("|     2：输入你所需页数（每页44个）     |")
+    print("===================================")
+    print("  输入你的关键字:")
+    key = raw_input()
+    print("  输入你所需页数（每页44个）:")
+    pagenum = raw_input()
+    try:
+        print("开始获取关键字为："+key+"的商品图片")
+        runlop = tbimage(key,pagenum)
+    except Exception as e:
+        print("error"+str(e))
+        print("================end================")
+
+
+
